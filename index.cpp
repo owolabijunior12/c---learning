ca6024fa95a841643f005b8c5e773b8058819101
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2024-01-18 11:51:47 +0100

    first commit

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string num1, num2;
+    cout << "Enter your first name:";
+    cin >> num1;
+    cout << "FIRST NAME : " << num1 << endl;
+    cout << "Enter your second name:";
+    cin >> num2;
+    cout << "SECOND NAME : " << num2 << endl;
+    cout << "FULL NAME : " << num1 << " " << num2;
+    return 0;
+}
